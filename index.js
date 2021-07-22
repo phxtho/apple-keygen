3c5eeaca684bb926f4b38ee4b04d192d7b4c5208
phxtho <phathopukwana@gmail.com>
2021-07-22 14:05:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let fs = require("fs");
+let jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+let privateKey = fs.readFileSync(process.env.privateKeyPath);
+let teamId = process.env.teamId;
+let clientId = process.env.clientId;
+let keyId = process.env.teamId;
+let validDays = 180; // In days. Max 180 (6 months) according to Apple docs.
+
+let token = jwt.sign(
+  {
+    iss: teamId,
+    iat: 0,
+    exp: 86400 * validDays,
+    aud: "https://appleid.apple.com",
+    sub: clientId,
+  },
+  privateKey,
+  {
+    algorithm: "ES256",
+    header: {
+      kid: keyId,
+    },
+  }
+);
+
+fs.writeFile(__dirname + "/clientSecret", token, () => {});
